958a40ce22450781a57281ddeb2736fe32243b16
Aleksey-1983 <0230595@mail.ru>
2022-07-26 22:55:17 +0300

    6.6.2. Двухмерный массив. Циклы. [#306725]

--- src/test/java/ru/job4j/array/MatrixSumTest.java
+++ src/test/java/ru/job4j/array/MatrixSumTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-class MatrixSumTest {
+public class MatrixSumTest {
     @Test
     public void whenSingle() {
         int[][] array = {{10}};
